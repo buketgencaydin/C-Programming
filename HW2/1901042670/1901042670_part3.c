4ef5ceb998fa9421f781533d65a59fd02b8281c6
buketgencaydin <57042485+buketgencaydin@users.noreply.github.com>
2020-07-21 19:17:35 +0300

    Update 1901042670_part3.c

--- HW2/1901042670/1901042670_part3.c
+++ HW2/1901042670/1901042670_part3.c
@@ -23,110 +23,85 @@ int main()
 	if(currency1==1 || currency1==2 || currency1==3)
 	{
 
-	switch(currency1)
-	{
-		case 1 :
-		printf("You have %lf Turkish Liras\n",amount);
-
-		printf("Choose which currency you want to convert\n");
-	    scanf("%d",&currency2);
-
-	    if(currency2==1 || currency2==2 || currency2==3)
-	    {
-	    
-		    switch(currency2)
-		    {
-		    	case 1 :
-			    printf("\nYou have %lf Turkish Liras\n",amount);
-			    break; 
-
-			    case 2 :
-			    printf("\nYou have %lf Euro\n",(amount/EURO));
-			    break;
-
-				case 3 :
-		        printf("\nYou have %lf Dollar\n",(amount/DOLLAR));
-		        break;
-		    }
-		}
-		else
+		switch(currency1)
 		{
-		    printf("\nYour selection is invalid!\n");	
-		}
-		break;
+			case 1 :
+			printf("You have %lf Turkish Liras\n",amount);
+			printf("Choose which currency you want to convert\n");
+		    	scanf("%d",&currency2);
 
-		case 2 :
-		printf("You have %lf Euro\n",amount);
-
-		printf("Choose which currency you want to convert\n");
-	    scanf("%d",&currency2);
-
-	    if(currency2==1 || currency2==2 || currency2==3)
-	    {
-	    
-
-			switch(currency2)
-		    {
-			    case 1 :
-			    printf("\nYou have %lf Turkish Liras\n",(amount*EURO));
-			    break;
-
-			    case 2 :
-		        printf("You have %lf Euro\n",amount);
-		        break;
-
-                case 3 :
-		        printf("\nYou have %lf Dollar\n",(amount*(EURO/DOLLAR)));
-		        break;
-		    }
-		}
-		else
-		{
-			printf("\nYour selection is invalid!\n");
-		}
-
-		 break;
-		
-
-        case 3 :
-		printf("You have %lf Dollar\n",amount);
-
-		printf("Choose which currency you want to convert\n");
-	    scanf("%d",&currency2);
-
-	    if(currency2==1 || currency2==2 || currency2==3)
-	    {
-	    
-
-			switch(currency2)
-		    {
-			    case 1 :
-			    printf("\nYou have %lf Turkish Liras\n",(amount*DOLLAR));
-			    break;
+		        if(currency2==1 || currency2==2 || currency2==3)
+		        {
+			    switch(currency2)
+			    {
+				case 1 :
+				    printf("\nYou have %lf Turkish Liras\n",amount);
+				    break; 
 
 				case 2 :
-		        printf("\nYou have %lf Euro\n",(amount*(DOLLAR/EURO)));
-		        break;
+				    printf("\nYou have %lf Euro\n",(amount/EURO));
+				    break;
 
-		        case 3 :
-		        printf("You have %lf Dollar\n",amount);
-		        break;
-            }
-        }
-        else
-        {
-        	printf("\nYour selection is invalid!\n");
-        }
-		break;
-   
-}
-}
- else
- {
- 	printf("\nYour selection is invalid!\n");
- }
+				case 3 :
+				    printf("\nYou have %lf Dollar\n",(amount/DOLLAR));
+				    break;
+			    }
+		    	 }
+		         else printf("\nYour selection is invalid!\n");	
+			 break;
 
+			 case 2 :
+				 printf("You have %lf Euro\n",amount);
+				 printf("Choose which currency you want to convert\n");
+				 scanf("%d",&currency2);
+
+				 if(currency2==1 || currency2==2 || currency2==3)
+				 {
+					 switch(currency2)
+					 {
+					    case 1 :
+						printf("\nYou have %lf Turkish Liras\n",(amount*EURO));
+						break;
+
+					    case 2 :
+						printf("You have %lf Euro\n",amount);
+						break;
+
+					    case 3 :
+						printf("\nYou have %lf Dollar\n",(amount*(EURO/DOLLAR)));
+						break;
+					  }
+				 }
+				 else printf("\nYour selection is invalid!\n");
+				 break;
+			    case 3 :
+				printf("You have %lf Dollar\n",amount);
+				printf("Choose which currency you want to convert\n");
+				scanf("%d",&currency2);
+
+			        if(currency2==1 || currency2==2 || currency2==3)
+			        {
+					switch(currency2)
+				    	{
+					    case 1 :
+						printf("\nYou have %lf Turkish Liras\n",(amount*DOLLAR));
+						break;
+
+					    case 2 :
+						printf("\nYou have %lf Euro\n",(amount*(DOLLAR/EURO)));
+						break;
+
+					    case 3 :
+						printf("You have %lf Dollar\n",amount);
+						break;
+			    		}
+				}
+				else printf("\nYour selection is invalid!\n");
+				break;
+
+		}
+	}
+        else printf("\nYour selection is invalid!\n");
+	
 	return 0;
-
-
 }
