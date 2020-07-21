0de6aeabb3a01c73e730a20a91cc0b1cc9681de2
buketgencaydin <57042485+buketgencaydin@users.noreply.github.com>
2020-07-21 19:52:18 +0300

    Update 1901042670_part2.c

--- HW3/1901042670_part2.c
+++ HW3/1901042670_part2.c
@@ -1,7 +1,8 @@
-/*  Since math.h library was used in the code, compile and run procedure need to be done like below :
-    $ gcc -c 1901042670_part2.c 
-	$ gcc 1901042670_part2.o -o 1901042670_part2 -lm
-	$ ./1901042670_part2
+/*  
+Since math.h library was used in the code, compile and run procedure need to be done like below :
+$ gcc -c 1901042670_part2.c 
+$ gcc 1901042670_part2.o -o 1901042670_part2 -lm
+$ ./1901042670_part2
 */
 
 #include <stdio.h>
@@ -18,34 +19,22 @@ int main()
 
 	for(i=2 ; i<a ; i++)
 	{
-		if(isprime(i)==1)
-		{
-			printf("%d is a prime number\n\n",i);
-		}
-		else
-		{
-			printf("%d is not a prime number, it is dividible by %d\n\n",i,isprime(i));
-		}
+		if(isprime(i)==1) printf("%d is a prime number\n\n",i);
+		else printf("%d is not a prime number, it is dividible by %d\n\n",i,isprime(i));
 	}
-
-	return (0);
+	return 0;
 }
-
 int isprime(int a)
 {
-    int x, counter;
+    int x, counter=0;
 
     for(x=2 ; x<=sqrt((double)a) ; x++)
+    {
+	if(a % x == 0 )
 	{
-		if(a % x == 0 )
-		{
-			counter++;
-			return (x);
-		}
-    }
-
-	if(counter == 0)
-	{
-		return (1);
+		counter++;
+		return (x);
 	}
+     }
+     if(counter == 0) return 1;
 }
